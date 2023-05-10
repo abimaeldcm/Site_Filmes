bf29499a665322f250e196fd2e93b8fbb3429f7e
Abimael Mendes <abimaelmends@hotmail.com>
2023-05-10 11:02:23 -0300

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } //O campo ID é necessário para o banco de dados para a chave primária.
+        public string? Title { get; set; }
+        [DataType(DataType.Date)] //Um atributo [DataType] que especifica o tipo de dados na ReleaseDate propriedade. O usuário não precisa inserir informações de hora no campo de data. Somente a data é exibida, não as informações de tempo.
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; } //O ponto de interrogação após string indica que a propriedade é anulável. 
+        public decimal price { get; set; }
+    }
+}
---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection(); // redireciona solicita��es HTTP para HTTPS.
+app.UseStaticFiles(); //permite que arquivos est�ticos, como HTML, CSS, imagens e JavaScript sejam atendidos.
+
+app.UseRouting(); //adiciona a correspond�ncia de rotas ao pipeline de middleware.
+
+app.UseAuthorization(); //autoriza um usu�rio a acessar recursos seguros.
+
+app.MapRazorPages(); //configura o roteamento de ponto de extremidade para Razor o Pages.
+
+app.Run(); //executa o aplicativo.
